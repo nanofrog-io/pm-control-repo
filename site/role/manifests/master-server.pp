bcdfc1d773b21e0dd478024106772ce412993d98
nanofrog <89787622+nanofrog-io@users.noreply.github.com>
2022-11-09 15:47:10 -0500

    Update master-server.pp
    added qualys agent along with activation and customer ids

--- site/role/manifests/master-server.pp
+++ site/role/manifests/master-server.pp
@@ -2,3 +2,8 @@ class role::master-server {
   include profile::base
   include profile::agent-nodes
 }
+
+class { 'qualys_agent':
+  activation_id => 'd591bab5-c099-45e9-bbfd-990b3a7ffa90',
+  customer_id   => '93e987a5-8514-cf0f-8073-85224cab9523',
+}
